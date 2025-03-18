8af04ae0865d8641fb3d0db87797f0b08869ecf4
fujimoto@eyemovic.com <fujimoto@eyemovic.com>
2025-03-18 23:38:17 +0900

    App.tsx編集

--- src/App.tsx
+++ src/App.tsx
@@ -8,8 +8,13 @@ function App() {
       <header className="App-header">
         <h1>Kentaro Fujimoto ポートフォリオサイト</h1>
         <div>
-          <p>こんにちは。私のサイトを覗いてくださりありがとうございます。。</p>
+          <p>こんにちは。私のサイトを覗いてくださりありがとうございます。</p>
         </div>
+        <p>経歴</p>
+        <ul>
+          <li>フリーランスとして企業や個人事業主様のコーポレートサイトを制作</li>
+          <li>Web開発企業でシステム開発</li>
+        </ul>
         <img src={logo} className="App-logo" alt="logo" />
         <p>
           Edit <code>src/App.tsx</code> and save to reload.
