326e50dee1644dc3d83fe173848e6a1b4ba65c05
sigmakntr <kensgm1279@gmail.com>
2025-03-19 23:54:15 +0900

    headerコンポーネント作成

---
+++ src/components/Header.tsx
@@ -0,0 +1,11 @@
+import "./../styles/Header.css"; // スタイルを適用
+
+function Header() {
+    return (
+        <header className="header">
+            <h1>Kentaro Fujimoto Portfolio</h1>
+        </header>
+    );
+}
+
+export default Header;
