265f7490445184f2b960ddf3989add9ff2a38c2a
sigmakntr <kensgm1279@gmail.com>
2025-03-22 19:58:28 +0900

    css調整

--- src/App.tsx
+++ src/App.tsx
@@ -7,59 +7,64 @@ import "./styles/app/style.css";
 import "./styles/main/style.css";
 
 function App() {
-    return (
-        <div className="app">
-            <div className="background"></div>
-            <div className="content">
-                <Header/>
-                <main>
-                    <section id="about" className="section-about">
-                        <h2>About</h2>
-                        <p>こんにちは。私のサイトを覗いてくださりありがとうございます。</p>
-                    </section>
+  return (
+    <div className="app">
+      <div className="background"></div>
+      <div className="content">
+        <Header/>
+        <main>
+          <section id="top" className="section-top">
+            <h1>KENTARO FUJIMOTO</h1>
+            <p>Code. Creativity. Curiosity.
+              This is the portfolio site of a software engineer.</p>
+          </section>
+          <section id="about" className="section-about">
+            <h2>About</h2>
+            <p>こんにちは。私のサイトを覗いてくださりありがとうございます。</p>
+          </section>
 
-                    <section id="works">
-                        <h2>Works</h2>
-                        <ul>
-                            <li>2022-2023：フリーランスとして企業や個人事業主様のコーポレートサイト制作</li>
-                            <li>2024-2025：Web開発会社でWebシステム開発やインフラ構築</li>
-                            <li>テスト</li>
-                            <li>テスト</li>
-                            <li>テスト</li>
-                            <li>テスト</li>
-                            <li>テスト</li>
-                            <li>テスト</li>
-                            <li>テスト</li>
-                            <li>テスト</li>
-                            <li>テスト</li>
-                            <li>テスト</li>
-                            <li>テスト</li>
-                            <li>テスト</li>
-                            <li>テスト</li>
-                            <li>テスト</li>
-                            <li>テスト</li>
-                            <li>テスト</li>
-                            <li>テスト</li>
-                            <li>テスト</li>
-                            <li>テスト</li>
-                            <li>テスト</li>
-                            <li>テスト</li>
-                            <li>テスト</li>
-                            <li>テスト</li>
-                            <li>テスト</li>
-                        </ul>
-                    </section>
+          <section id="works">
+            <h2>Works</h2>
+            <ul>
+              <li>2022-2023：フリーランスとして企業や個人事業主様のコーポレートサイト制作</li>
+              <li>2024-2025：Web開発会社でWebシステム開発やインフラ構築</li>
+              <li>テスト</li>
+              <li>テスト</li>
+              <li>テスト</li>
+              <li>テスト</li>
+              <li>テスト</li>
+              <li>テスト</li>
+              <li>テスト</li>
+              <li>テスト</li>
+              <li>テスト</li>
+              <li>テスト</li>
+              <li>テスト</li>
+              <li>テスト</li>
+              <li>テスト</li>
+              <li>テスト</li>
+              <li>テスト</li>
+              <li>テスト</li>
+              <li>テスト</li>
+              <li>テスト</li>
+              <li>テスト</li>
+              <li>テスト</li>
+              <li>テスト</li>
+              <li>テスト</li>
+              <li>テスト</li>
+              <li>テスト</li>
+            </ul>
+          </section>
 
-                    <section id="contact">
-                        <h2>Contact</h2>
-                        <p>お問い合わせは以下のメールまでお願いします。</p>
-                        <p><a href={`mailto:${code.mails.contact}`}>{code.mails.contact}</a></p>
-                    </section>
-                </main>
-                <Footer />
-            </div>
-        </div>
-    );
+          <section id="contact">
+            <h2>Contact</h2>
+            <p>お問い合わせは以下のメールまでお願いします。</p>
+            <p><a href={`mailto:${code.mails.contact}`}>{code.mails.contact}</a></p>
+          </section>
+        </main>
+        <Footer />
+      </div>
+    </div>
+  );
 }
 
 export default App;
